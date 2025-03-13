875d6473b9169bba5208e0ff6d2a6b7cc076a3ec
[sujinpsujinp] <sujinp038@naver.com>
2025-03-13 16:47:48 +0900

    0313 확인

--- src/main/java/data/controller/SawonController.java
+++ src/main/java/data/controller/SawonController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SawonController {
 	@GetMapping({"/","/list"})
-	public String sawonList()
+	public String sawonlist()
 	{
 		return "sawon/sawonlist";		
 	}
